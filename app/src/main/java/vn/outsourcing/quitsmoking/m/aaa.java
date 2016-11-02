f63d3ccc77c24981462fc30346b501a48c1c12bf
vietruyn <vietruyn@gmail.com>
2016-11-02 21:17:32 +0700

    Initial commit

---
+++ app/src/main/java/vn/outsourcing/quitsmoking/m/aaa.java
@@ -0,0 +1,7 @@
+package vn.outsourcing.quitsmoking.m;
+
+/**
+ * Created by VietRuyn on 26/10/2016.
+ */
+public class aaa {
+}
---
+++ app/src/main/java/vn/outsourcing/quitsmoking/v/adapter/aaaa.java
@@ -0,0 +1,7 @@
+package vn.outsourcing.quitsmoking.v.adapter;
+
+/**
+ * Created by VietRuyn on 26/10/2016.
+ */
+public class aaaa {
+}
